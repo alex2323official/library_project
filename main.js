cd58f16ff67c46dba488f9bea6295fa51c8acadf
Alex <alex2323official@gmail.com>
2024-09-12 14:02:33 +0200

    I can show single iteam - before looping trough array

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const btn = document.querySelector("#button");
+
 function Book(author, title, numberOfPages) {
   // the constructor...
   this.author = author;
@@ -12,7 +14,21 @@ function addBookToLibrary() {
 }
 
 const hobbit = new Book("Tolkien", "Hobbit", 300);
+const opowiesciMasla = new Book("Maslo", "Opowiesci masla", 300);
 
 myLibrary.push(hobbit);
+myLibrary.push(opowiesciMasla);
 
 console.table(myLibrary);
+
+// Display Books in Container
+const booksContainerDisplay = document.querySelector("#books-container");
+
+// Create new html element with book
+let newBookContainerDiv = document.createElement("div");
+let authorSpan = document.createElement("span");
+authorSpan.textContent = `${myLibrary[0].author}`;
+
+newBookContainerDiv.appendChild(authorSpan);
+
+booksContainerDisplay.appendChild(newBookContainerDiv);
