861bd08564fef28b6c53ece67c58c60350b9fa67
Alex <alex2323official@gmail.com>
2024-09-12 14:23:22 +0200

    UI for books dispaly works

--- main.js
+++ main.js
@@ -15,8 +15,16 @@ function addBookToLibrary() {
 
 const hobbit = new Book("Tolkien", "Hobbit", 300);
 const opowiesciMasla = new Book("Maslo", "Opowiesci masla", 300);
+const hobbit2 = new Book("Tolkien", "Hobbit", 300);
+const hobbit3 = new Book("Tolkien", "Hobbit", 300);
+const hobbit4 = new Book("Tolkien", "Hobbit", 300);
+const hobbit5 = new Book("Tolkien", "Hobbit", 300);
 
 myLibrary.push(hobbit);
+myLibrary.push(hobbit2);
+myLibrary.push(hobbit3);
+myLibrary.push(hobbit4);
+myLibrary.push(hobbit5);
 myLibrary.push(opowiesciMasla);
 
 // Display Books in Container
@@ -24,8 +32,6 @@ const booksContainerDisplay = document.querySelector("#books-container");
 
 // Loop trough array and display every book
 myLibrary.forEach((book) => {
-  console.log(book);
-
   // Create new html element with book
   let newBookContainerDiv = document.createElement("div");
 
