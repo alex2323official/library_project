5181dcdb09b7633c4f0ebe5306d8a364571b249d
Alex <alex2323official@gmail.com>
2024-09-12 13:26:31 +0200

    basic structure before goin crazy

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("howdy");
+const myLibrary = [];
+
+function Book(author, title, numberOfPages) {
+  // the constructor...
+  this.author = author;
+  this.title = title;
+  this.numberOfPages = numberOfPages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+const hobbit = new Book("Tolkien", "Hobbit", 300);
+
+myLibrary.push(hobbit);
+
+console.table(myLibrary);
